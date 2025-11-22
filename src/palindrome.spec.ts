bfe20f0414428239a8ce63b7812a8bd496d525d7
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-11-23 00:05:20 +0200

    test: use it.each() to test on set of words/phrases and improve readibility

--- src/palindrome.spec.ts
+++ src/palindrome.spec.ts
@@ -1,23 +1,25 @@
 import { PalindromeChecker } from "./palindrome";
 
 describe("Palindrome checker", () => {
-  it("knows that 'mom' is palindrome", () => {
-    expect(PalindromeChecker.isPalindrome("mom")).toBe(true);
+  it.each(["mom", "wow", "level"])("knows single word palindrome", (str) => {
+    expect(PalindromeChecker.isPalindrome(str)).toBe(true);
   });
 
-  it("knows that 'bill' is not palindrome", () => {
-    expect(PalindromeChecker.isPalindrome("bill")).toBe(false);
+  it.each(["bill", "money", "fuck"])(
+    "knows non-palindrome single word",
+    (str) => {
+      expect(PalindromeChecker.isPalindrome(str)).toBe(false);
+    }
+  );
+
+  it.each(["Mom", "WoW", "LEVEL"])("knows off casing palindrome", (str) => {
+    expect(PalindromeChecker.isPalindrome(str)).toBe(true);
   });
 
-  it("knows off case palindromes such as 'Mom'", () => {
-    expect(PalindromeChecker.isPalindrome("Mom")).toBe(true);
-  });
-
-  it("knows that 'Was It A Rat I Saw' is a palindrome", () => {
-    expect(PalindromeChecker.isPalindrome("Was It A Rat I Saw")).toBe(true);
-  });
-
-  it('should be able to tell that "Never Odd or Even" is palindrome', () => {
-    expect(PalindromeChecker.isPalindrome("Never Odd or Even")).toBe(true);
-  });
+  it.each(["Was It A Rat I Saw", "Never Odd or Even"])(
+    "knows palindrome in phrases",
+    (str) => {
+      expect(PalindromeChecker.isPalindrome(str)).toBe(true);
+    }
+  );
 });
